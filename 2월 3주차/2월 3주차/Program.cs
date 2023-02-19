839836f2bbe40767ce96c03326d13cf14bd88403
jiyoung1234 <qkrwldud131@naver.com>
2023-02-19 10:36:48 +0900

    01 빠짐

--- 2월 3주차/2월 3주차/Program.cs
+++ 2월 3주차/2월 3주차/Program.cs
@@ -8,8 +8,59 @@ namespace _2월_3주차
 {
     class Program
     {
+        private static int plus(int s, int t)
+        {
+            int resultnumber = s + t;
+            return resultnumber;
+        }
+        private static int minus(int s, int t)
+        {
+            int resultnumber = s - t;
+            return resultnumber;
+        }
+        private static int multipl(int s, int t)
+        {
+            int resultnumber = s * t;
+            return resultnumber;
+        }
+        private static int div(int s, int t)
+        {
+            int resultnumber = s / t;
+            return resultnumber;
+        }
         static void Main(string[] args)
         {
+            string nn1, nn2;
+            nn1 = System.Console.ReadLine();
+            int s = Convert.ToInt32(nn1);
+            string s3 = System.Console.ReadLine();
+            nn2 = System.Console.ReadLine();
+            int t = Convert.ToInt32(nn2);
+
+            if (s3 == "+")
+            {
+                int NewInt = plus(s, t);
+                System.Console.WriteLine(NewInt);
+            }
+            else if (s3 == "-")
+            {
+                int NewInt = minus(s, t);
+                System.Console.WriteLine(NewInt);
+            }
+            else if (s3 == "*")
+            {
+                int NewInt = multipl(s, t);
+                System.Console.WriteLine(NewInt);
+            }
+            else if (s3 == "/")
+            {
+                int NewInt = div(s, t);
+                System.Console.WriteLine(NewInt);
+            }
+            else
+            {
+                System.Console.WriteLine("잘못된 부호입니다.");
+            }
         }
     }
 }
